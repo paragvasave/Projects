b081a1a15887e546cd7fc05df99a1d6ddd0e0ed7
paragvasave <124817557+paragvasave@users.noreply.github.com>
2023-03-30 13:14:18 +0530

    Initial commit

---
+++ Final Packer Unpacker/Demo/Program401.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Program401
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj=new Scanner(System.in);
+
+        System.out.println("Enter file name");
+        String FileName=sobj.nextLine();
+
+        System.out.println("File name is : "+FileName);
+    }
+
+}
